6bf667b17e3022f48668ed740fe47fa9417db3c8
3dagger <dlqtn36@gmail.com>
2024-02-13 22:46:07 +0900

    Update: Android minimum sdk version
    - 21 > 26

--- build-logic/convention/src/main/kotlin/kr/dagger/nuyhoostmdb/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/kr/dagger/nuyhoostmdb/KotlinAndroid.kt
@@ -5,7 +5,6 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -17,7 +16,7 @@ internal fun Project.configureKotlinAndroid(
 		compileSdk = 34
 
 		defaultConfig {
-			minSdk = 21
+			minSdk = 26
 		}
 
 		compileOptions {
